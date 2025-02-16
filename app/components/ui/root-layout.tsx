4bc224f85e23f7914cff90528979f83e234978e5
Pavel Lozinsky <pavel@lozinsky.dev>
2025-02-16 13:05:52 +0300

    feat: change root layout breakpoints

--- app/components/ui/root-layout.tsx
+++ app/components/ui/root-layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 export function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='xs:max-w-sm relative mx-auto grid h-full grid-rows-[auto_1fr] px-6 sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl'>
+    <div className='xs:max-w-xs relative mx-auto grid h-full grid-rows-[auto_1fr] px-6 sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl'>
       {children}
     </div>
   );
